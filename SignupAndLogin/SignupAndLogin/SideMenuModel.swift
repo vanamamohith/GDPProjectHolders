96c4d5ee5cb3df85ad4cbbda7325cce544ce0e06
Mohith Vanama <30409142+vanamamohith@users.noreply.github.com>
2022-04-20 14:02:59 -0500

    Backend connections for Signin and login Updated

---
+++ SignupAndLogin/SignupAndLogin/SideMenuModel.swift
@@ -0,0 +1,13 @@
+//
+//  SideMenuModel.swift
+//  SignupAndLogin
+//
+//  Created by student on 3/25/22.
+//
+
+import Foundation
+
+struct SideMenuModel {
+    var icon: UIImage
+    var title: String
+}
---
+++ SignupAndLogin/SignupAndLogin/forgotPasswordViewController.swift
@@ -0,0 +1,43 @@
+//
+//  forgotPasswordViewController.swift
+//  SignupAndLogin
+//
+//  Created by student on 3/18/22.
+//
+
+import UIKit
+
+class forgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        let storyboard = UIStoryboard(name : "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "otpoption")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    
+    @IBAction func submit(_ sender: Any) {
+        let storyboard = UIStoryboard(name : "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Login")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
